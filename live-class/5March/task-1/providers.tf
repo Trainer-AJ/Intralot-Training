e376b0e8423f4ea07c8ca086315b1690bf86c9f3
AJ <90054101+Trainer-AJ@users.noreply.github.com>
2025-03-05 15:26:04 +0530

    Create providers.tf

---
+++ live-class/5March/task-1/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+ required_version = "~> 1.11"
+ 
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 4.21"
+    }
+  }
+}
+ 
+provider "azurerm" {
+  # Configuration options
+  features {}
+  client_id = "YOUR-VALUES-HERE"
+  client_secret = "YOUR-VALUES-HERE"
+  tenant_id = "YOUR-VALUES-HERE"
+  subscription_id = "YOUR-VALUES-HERE"
+}
