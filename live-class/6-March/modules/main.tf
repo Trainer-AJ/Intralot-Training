e4f865a2b1644be61f02e3997ff6abd229228ba3
AJ <90054101+Trainer-AJ@users.noreply.github.com>
2025-03-06 14:31:02 +0200

    Create main.tf

---
+++ live-class/6-March/modules/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.21.1"
+    }
+  }
+
+}
+
+
+
+provider "azurerm" {
+  features {}
+}
+
+
+module "nickname" {
+  source = "github.com/Trainer-AJ/Intralot-Training/labs/codes/modules-demo//blob_static_site?ref=main"
+  
+  index_document        = "../content/index.html"
+  error_404_document    = "../content/custom_error.html"
+}
+
+
+output "url" {
+  value = module.nickname.primary_web_host
+  
+}
