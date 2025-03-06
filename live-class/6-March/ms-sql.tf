0eb5029538b88c1cd44fa55382556224b5b4a3e0
AJ <90054101+Trainer-AJ@users.noreply.github.com>
2025-03-06 11:51:49 +0200

    Update ms-sql.tf

--- live-class/6-March/ms-sql.tf
+++ live-class/6-March/ms-sql.tf
@@ -1,5 +1,16 @@
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "unique_storage_account_name" {
+  length  = 5
+  special = false
+  upper   = false
+  numeric = true
+}
+
 resource "azurerm_mssql_server" "example" {
-  name                         = "mssqlserver89749854"
+  name                         = "mssqlserver-${random_string.unique_storage_account_name.result}"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
