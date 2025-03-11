fad5edbf459c3cd75d9aac4d5e216560c76a5a90
trainer-aj <ananay.wfhintellify@gmail.com>
2025-03-11 11:58:36 +0200

    done

--- live-class/11-march/variables-demo/approach-1/variables.tf
+++ live-class/11-march/variables-demo/approach-1/variables.tf
@@ -1,11 +1,12 @@
 # since NO Value specified, it will ask for value during terraform plan/apply
 variable "region" {
   description = "give azure region name to deploy resources"
+  default = "East US"
 }
 
 variable "rg_name" {
   description = "give azure resource group name"
-  default = "rg-aci"
+  # default = "rg-aci"
   
 }
 
