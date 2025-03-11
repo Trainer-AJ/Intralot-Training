fc5b1fce3d81f2d8a86ad5fc83e56fb41905edac
AJ <90054101+Trainer-AJ@users.noreply.github.com>
2025-03-11 10:17:45 +0200

    Create terraform-settings.tf

---
+++ live-class/11-march/terraform-settings.tf
@@ -0,0 +1,20 @@
+###########################################################
+# This is just a checklist for terraform to decide 
+# if to RUN the code 
+# OR
+# Exit with error
+############################################################
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 4.21.0"
+    }
+  }
+  # version of terraform required
+  required_version = "1.11.0"
+}
+
+provider "azurerm" {
+  # Configuration options
+}
