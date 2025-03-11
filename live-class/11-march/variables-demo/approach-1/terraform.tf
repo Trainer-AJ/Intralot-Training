4c24cccaba8d3c51e188e4c401f1d27fb7715c70
AJ <90054101+Trainer-AJ@users.noreply.github.com>
2025-03-11 11:33:56 +0200

    Add files via upload

---
+++ live-class/11-march/variables-demo/approach-1/terraform.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 4.21.0"
+    }
+  }
+  # version of terraform required
+  required_version = "1.11.0"
+}
---
+++ live-class/11-march/variables-demo/approach-1/variables.tf
@@ -0,0 +1,11 @@
+# since NO Value specified, it will ask for value during terraform plan/apply
+variable "region" {
+  description = "give azure region name to deploy resources"
+}
+
+variable "rg_name" {
+  description = "give azure resource group name"
+  default = "rg-aci"
+  
+}
+
